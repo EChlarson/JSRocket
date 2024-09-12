a2409b4556007efdb5475aed4c90263a292e39a8
EChlarson <chl20001@byui.edu>
2024-09-11 22:34:57 -0500

    Exp start

---
+++ explanations.js
@@ -0,0 +1,23 @@
+// Explanations
+
+//Variables
+var favColor = 'red';
+   //variable is something that refers to something else
+   //names can not have spaces or dashes
+   //value is assigned 'red' but can be changed
+   
+   // Types of Values 
+      //string or text ''
+      //Array or List (could contain text, numbers, variables, objects, functions, etc.)
+var myArray = [];
+var myFavColors = ['blue', 'red', 'green'];
+      //Boolean is a true or false statement or yes no value (0is false 1 is true)
+var hasGotFavColors = true;
+      //Object is like an array but you can give it a name for every list item
+var richObject = {
+   firstName: 'Rich',
+   lastName: 'Armstrong',
+   favColors: ['blue', 'red', 'green'],
+   yearsAlive: 103,
+   isMale: true
+};
